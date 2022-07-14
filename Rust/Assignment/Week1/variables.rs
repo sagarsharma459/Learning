5dea48316303bf3d73a7c70744be3543a6b59e76
sagarsharma459 <106689057+sagarsharma459@users.noreply.github.com>
2022-07-14 17:49:59 +0530

    Update and rename variables.md to variables.rs

---
+++ Rust/Assignment/Week1/variables.rs
@@ -0,0 +1,35 @@
+// Questions 1) : A variable can be used only if it has been initialized.
+/* Fix the error below with least amount of modification to the code
+fn main() {
+    let x: i32; // Uninitialized but used, ERROR !
+    let y: i32; // Uninitialized but also unused, only a Warning !
+
+    assert_eq!(x, 5);
+    println!("Success!");
+}
+*/
+
+Solutions :
+
+fn main() {
+    let x: i32 = 5; // Uninitialized but used, ERROR !
+    let _y: i32; // Uninitialized but also unused, only a Warning !
+
+    assert_eq!(x, 5);
+    println!("Success!");
+}
+
+Output : Success!
+
+
+// Question 2) : Use mut to mark a variable as mutable.
+/*
+// Fix the error below with least amount of modification to the code
+fn main() {
+    let x: i32 = 5; // Uninitialized but used, ERROR !
+    let _y: i32; // Uninitialized but also unused, only a Warning !
+
+    assert_eq!(x, 5);
+    println!("Success!");
+}
+*/
