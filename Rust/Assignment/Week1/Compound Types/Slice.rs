3d3798974a39ddac7b13c9fdf10d4b416c7d6555
sagarsharma459 <106689057+sagarsharma459@users.noreply.github.com>
2022-07-15 09:57:17 +0530

    Update Slice.rs

--- Rust/Assignment/Week1/Compound Types/Slice.rs
+++ Rust/Assignment/Week1/Compound Types/Slice.rs
@@ -32,7 +32,7 @@ output: Success!
 
     let slice = &arr[..2];
     
-    // Modify '6' to make it work
+    // Modify '6' to make it work 
     // TIPS: slice( reference ) IS NOT an array, if it is an array, then `assert!` will passed: Each of the two UTF-8 chars '中' and '国'  occupies 3 bytes, 2 * 3 = 6
     assert!(std::mem::size_of_val(&slice) == 6);
 
