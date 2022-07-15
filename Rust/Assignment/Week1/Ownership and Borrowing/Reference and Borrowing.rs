cdeee37a13e67925a561e1403c7793b05b23dc65
sagarsharma459 <106689057+sagarsharma459@users.noreply.github.com>
2022-07-15 09:27:11 +0530

    Update Reference and Borrowing.rs

--- Rust/Assignment/Week1/Ownership and Borrowing/Reference and Borrowing.rs
+++ Rust/Assignment/Week1/Ownership and Borrowing/Reference and Borrowing.rs
@@ -8,3 +8,312 @@
    println!("the memory address of x is {:p}", p); // One possible output: 0x16fa3ac84
 } */
 
+Solutions : 
+
+fn main() {
+   let x = 5;
+   // Fill the blank
+   let p = &x;
+
+   println!("the memory address of x is {:p}", p); // One possible output: 0x16fa3ac84
+}
+
+Output : the memory address of x is 0x7ffd2f7045fc
+
+2) 
+
+/* fn main() {
+    let x = 5;
+    let y = &x;
+
+    // Modify this line only
+    assert_eq!(5, y);
+
+    println!("Success!");
+} */
+
+Solutions: 
+
+fn main() {
+    let x = 5;
+    let y = &x;
+
+    // Modify this line only
+    assert_eq!(5, *y);
+
+    println!("Success!");
+}
+
+Output : Success!
+
+3) 
+
+/* // Fix error
+fn main() {
+    let mut s = String::from("hello, ");
+
+    borrow_object(s);
+
+    println!("Success!");
+}
+
+fn borrow_object(s: &String) {} */
+
+Solutions: 
+
+fn main() {
+    let mut s = String::from("hello, ");
+
+    borrow_object(&s);
+
+    println!("Success!");
+}
+
+fn borrow_object(s: &String) {}
+
+Output: Success!
+
+4) 
+
+/* // Fix error
+fn main() {
+    let mut s = String::from("hello, ");
+
+    push_str(s);
+
+    println!("Success!");
+}
+
+fn push_str(s: &mut String) {
+    s.push_str("world")
+} */
+
+Soultions: 
+
+fn main() {
+    let mut s = String::from("hello, ");
+
+    push_str(&mut s);
+
+    println!("Success!");
+}
+
+fn push_str(s: &mut String) {
+    s.push_str("world")
+}
+
+output: Success!
+
+5) 
+
+/* fn main() {
+    let mut s = String::from("hello, ");
+
+    // Fill the blank to make it work
+    let p = __;
+    
+    p.push_str("world");
+
+    println!("Success!");
+} */
+
+Solutions: 
+
+fn main() {
+    let mut s = String::from("hello, ");
+
+    let p = &mut s;
+    
+    p.push_str("world");
+
+    println!("Success!");
+}
+
+
+output: Success!
+
+6) 
+
+/* fn main() {
+    let c = '中';
+
+    let r1 = &c;
+    // Fill the blank，dont change other code
+    let __ r2 = c;
+
+    assert_eq!(*r1, *r2);
+    
+    // Check the equality of the two address strings
+    assert_eq!(get_addr(r1),get_addr(r2));
+
+    println!("Success!");
+}
+
+// Get memory address string
+fn get_addr(r: &char) -> String {
+    format!("{:p}", r)
+} */
+
+Solutions: 
+
+fn main() {
+    let c = '中';
+
+    let r1 = &c;
+    let ref r2 = c;
+
+    assert_eq!(*r1, *r2);
+    
+    assert_eq!(get_addr(r1),get_addr(r2));
+
+    println!("Success!");
+}
+
+fn get_addr(r: &char) -> String {
+    format!("{:p}", r)
+}
+
+output: Success!
+
+7) 
+
+/* // Remove something to make it work
+// Don't remove a whole line !
+fn main() {
+    let mut s = String::from("hello");
+
+    let r1 = &mut s;
+    let r2 = &mut s;
+
+    println!("{}, {}", r1, r2);
+
+    println!("Success!");
+} */
+
+Solutions: 
+
+fn main() {
+    let mut s = String::from("hello");
+
+    let r1 = &s;
+    let r2 = &s;
+
+    println!("{}, {}", r1, r2);
+
+    println!("Success!");
+}
+
+
+output: 
+hello, hello
+Success!
+
+8) 
+
+/* n main() {
+    // Fix error by modifying this line
+    let  s = String::from("hello, ");
+
+    borrow_object(&mut s);
+
+    println!("Success!");
+}
+
+fn borrow_object(s: &mut String) {} */
+
+Solutions: 
+
+fn main() {
+
+    let mut s = String::from("hello, ");
+
+    borrow_object(&mut s);
+
+    println!("Success!");
+}
+
+fn borrow_object(s: &mut String) {}
+
+output: Success!
+
+9) 
+
+/* // This code has no errors!
+fn main() {
+    let mut s = String::from("hello, ");
+
+    borrow_object(&s);
+    
+    s.push_str("world");
+
+    println!("Success!");
+}
+
+fn borrow_object(s: &String) {} */
+
+Solutions: 
+
+fn main() {
+    let mut s = String::from("hello, ");
+
+    borrow_object(&mut s);
+    
+    s.push_str("world");
+
+    println!("Success!");
+}
+
+fn borrow_object(s: &String) {}
+
+output: Success!
+
+10) 
+
+/* // Comment one line to make it work
+fn main() {
+    let mut s = String::from("hello, ");
+
+    let r1 = &mut s;
+    r1.push_str("world");
+    let r2 = &mut s;
+    r2.push_str("!");
+    
+    println!("{}",r1);
+} */
+
+Solutions: 
+
+fn main() {
+    let mut s = String::from("hello, ");
+
+    let r1 = &mut s;
+    r1.push_str("world");
+    let r2 = &mut s;
+    r2.push_str("!");
+    
+   // println!("{}",r1);
+}
+
+11) 
+/* fn main() {
+    let mut s = String::from("hello, ");
+
+    let r1 = &mut s;
+    let r2 = &mut s;
+
+    // Add one line below to make a compiler error: cannot borrow `s` as mutable more than once at a time
+    // You can't use r1 and r2 at the same time
+} */
+
+Solutions: 
+
+fn main() {
+    let mut s = String::from("hello, ");
+
+    let r1 = &mut s;
+    // let r2 = &mut s;
+
+    r1.push_str("world");
+}
+
+
